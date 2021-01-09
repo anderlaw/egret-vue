bf51a6b15868cd27bd8df3bdea8c0a8ec8e7ae27
andrew <andrew@it.ddileaf.com>
2021-01-09 23:19:51 +0800

    删除测试组件

--- src/package/components/PartOneRect.ts
+++
@@ -1,9 +0,0 @@
-class PartOneRect extends eui.Component {
-  constructor() {
-    super();
-    //设置一个背景图
-    const label = new eui.Label();
-    label.text = "这里是partonerect页面";
-    this.addChild(label);
-  }
-}
