e58482e5b4a7bc5a2cc7c3d16268645955b5f936
andrew <andrew@it.ddileaf.com>
2021-01-20 16:33:31 +0800

    修改组件的生命周期名字

--- src/package/PageComponent.ts
+++ src/package/PageComponent.ts
@@ -1,23 +1,30 @@
 namespace DDI {
-    export class PageComponent extends eui.Component{
-    app: App;
-    router: Router;
-    public content_label: eui.Label;
+  export class PageComponent extends eui.Component {
+    $app: App;
+    $router: Router;
     constructor(app: App, router: Router) {
       super();
-      this.router = router;
-      this.app = app;
-      this.UIWillInit();
-      this.addEventListener(egret.Event.ADDED_TO_STAGE,this.mounted,this)
+      this.$router = router;
+      this.$app = app;
+      this.componentWillInit();
+      this.addEventListener(egret.Event.ADDED_TO_STAGE, this.mounted, this);
+      this.addEventListener(
+        egret.Event.REMOVED_FROM_STAGE,
+        this.unmounted,
+        this
+      );
     }
     /**
      * 界面UI的准备工作（设置皮肤名称、创建元素、添加元素等）的钩子
      */
-    public UIWillInit(){}
+    public componentWillInit() {}
     /**
      * 组件添加到舞台（stage）时的钩子
      */
-    public mounted(){}
-
-}
+    public mounted() {}
+    /**
+     * 组件从舞台移除
+     */
+    public unmounted() {}
+  }
 }
